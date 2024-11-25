576ac18d63cd6c332f7949b6f93ad54a67bef29a
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-25 19:24:11 +0600

    Update Array Searching Algorithm by Linear Search.c

--- Array Searching Algorithm by Linear Search.c
+++ Array Searching Algorithm by Linear Search.c
@@ -1,22 +1,32 @@
-#include <stdio.h>   
-int linearSearch(int arr[], int n, int target) {  
-    int i;  
-    for (i = 0; i< n; i++) {  
-        if (arr[i] == target) {  
-            return i;
-        }  
-    }  
-    return -1;
-}  
-int main() {  
-    int arr[] = {10, 2, 8, 5, 17};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    int target = 8;  
-    int result = linearSearch(arr, n, target);  
-    if (result == -1) {  
-printf("Element not found in the array.\n");  
-    } else {  
-printf("The target Element %d is found at index: %d\n", target,result);  
-    }  
-    return 0;  
-} 
+#include <stdio.h>
+int main(){
+int arr[100], i, size, value,index, count=0;
+printf("Enter Array Size: ");
+scanf("%d",&size);
+printf("Enter %d Array Element: ", size);
+for(i=0;i<size;i++)
+{
+    scanf("%d", &arr[i]);
+}
+printf("The Array Element Are: \n");
+    for(i=0;i<size;i++){
+        printf("%d ", arr[i]); 
+    }
+printf("\nEnter the element you want to search for: ");
+scanf("%d", &value);
+for(i=0;i<size;i++)
+{
+    if (arr[i]==value)
+    {
+        index=i;
+        count++;
+        break;
+    }
+}
+    if (count==1)
+    {
+        printf("The %d Element is Found and The Index is %d \n",value,index );
+    }
+        else printf("Element is not Found\n");
+    return 0;
+}
